755aba38ef704287ff45be932c18259e415fe3d8
nomansadiq <noman@evvafinanzas.com>
2020-08-25 00:37:30 +0200

    App.js changes

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         This is test app
         </a>
       </header>
     </div>
