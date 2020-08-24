94132f0e2a1f6fcf103717544a41025636cc944d
noman-evva <69483968+noman-evva@users.noreply.github.com>
2020-08-25 01:23:26 +0200

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,10 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
+
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
- 
+ const core = require('@actions/core') 
+  const token = process.env.API_KEY
+console.log('token', token)
 });
